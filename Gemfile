3b34f460a6ee000ed8ad49608362d26190ccf267
papi-gorilla <jindeensadayoshi@gmail.com>
2021-05-09 03:21:38 +0000

    [Finish] 週初登録機能の追加

--- Gemfile
+++ Gemfile
@@ -73,3 +73,5 @@ gem "refile-mini_magick"
 
 gem 'bootstrap', '~> 4.5'
 gem 'jquery-rails'
+
+gem "jp_prefecture"
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,4 +18,5 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require jquery
 //= require_tree .
